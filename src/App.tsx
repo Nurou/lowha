359e2f893652a7729ecc4d95cdb1aed1503943e8
Joel Hassan <joel.nhn@gmail.com>
2024-08-11 23:39:49 +0300

    fix: clear audio when switching modes

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,15 @@ function App() {
     <div className={cx('grid place-items-center p-8 bg-stone-200 h-full', "bg-[url('/moroccan.svg')]")}>
       <h1 className='text-4xl font-bold text-stone-600 py-8'>{'\ufdfd'}</h1>
       <div className='grid place-items-center py-4 text-2xl'>
-        <ModeSelect modes={modes} defaultMode={DEFAULT_MODE} changeMode={setSelectedMode} />
+        <ModeSelect
+          modes={modes}
+          defaultMode={DEFAULT_MODE}
+          changeMode={(mode) => {
+            setSelectedMode(mode);
+            pauseSound(currentlyPlayingRef);
+            setCurrentlyPlayingLetter(null);
+          }}
+        />
       </div>
       <div className='border-4 border-stone-100 rounded-md p-8'>
         <div dir='rtl' className='grid place-items-center md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-8'>
